10e03e137d044885b84a8209437365d6c2c49639
eddt <etwilbeck@marketingincolor.com>
2016-06-14 14:03:37 -0500

    Added FB icon, closes #1

--- wp-content/themes/theme55705/assets/js/my_script.js
+++ wp-content/themes/theme55705/assets/js/my_script.js
@@ -5,17 +5,22 @@
 		$('#menu-primary>#menu-primary-items>li>a').each(function(){
 	        var $this = $(this),
 	            txt = $this.text();
-		        $this.html('<div class="txt_menu"><span>'+ txt +'</span></div><div class="txt_menu"><span>'+ txt +'</span></div>');
+				if (txt === "fb") {
+					$this.html('<div class="txt_menu"><span><i class="fa fa-facebook-square"></i></span></div><div class="txt_menu"><span><i class="fa fa-facebook-square"></i></span></div>');
+				} else {
+					$this.html('<div class="txt_menu"><span>'+ txt +'</span></div><div class="txt_menu"><span>'+ txt +'</span></div>');
+				}
+		        //$this.html('<div class="txt_menu"><span>'+ txt +'</span></div><div class="txt_menu"><span>'+ txt +'</span></div>');
 		});
 
 		//$('.cherry-navigation-arrow').each(function(){
 			$('.cherry-navigation-arrow').find('.menu-item-has-children').each(function(){
 				_this = $(this),
 				_this.find(".txt_menu").append('<div class="arrow_mov"></div>');
-			});	
+			});
 		//});
 
-		
+
 		$('.post.type-post').each(function(){
 			_this = $(this);
 			_this.find('.entry-thumbnail').insertAfter(_this.find('.entry-header'));
@@ -26,13 +31,13 @@
 
 		    	//$('.static-footer-sidebar5').width($(window).width());
 				//$('.static-footer-sidebar5').css({width: $(window).width(), "margin-left": ($(window).width()/-2), 'left':'50%'});
-			
+
 		   }
 
 		).trigger('resize');
-		
+
 
 	});
 
- 
+
 })(jQuery);
